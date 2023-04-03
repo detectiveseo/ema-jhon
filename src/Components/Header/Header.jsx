10c39516a8b8f2bd1c76d4a1c13a124e8e85a06b
detectiveseo <detectiveseo1@gmail.com>
2023-04-03 11:27:06 +0600

    added header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='bg-slate-700 h-20 flex items-center justify-between px-40'>
+            <div className="logo">
+                <img src={Logo} alt="" />
+            </div>
+            <nav className='grid grid-flow-col gap-10 text-white list-none'>
+                <li><a href="">Order</a></li>
+                <li><a href="">Order Review</a></li>
+                <li><a href="">Manage Inventory</a></li>
+                <li><a href="">Login</a></li>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
